28b86fb337c303f4f2b785ee40909999db1a79be
Pasteras <128259268+Pasteras@users.noreply.github.com>
2023-03-25 22:26:29 +0200

    Add files via upload

---
+++ Task2/H_m15_Task2.cpp
@@ -0,0 +1,44 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    const int SIZE = 4;
+    int mas[SIZE][SIZE]{};
+    int num;
+    int num_a;
+    for (int i = 0; i < SIZE; i++)
+    {
+        for (int j = 0; j < SIZE; j++)
+        {
+            if (j == 0)//For the first number of each array
+            {
+                cout << "Press num: ";
+                cin >> num;
+                mas[i][j] = num;
+            }
+            if (j >= 1)
+            {
+                num_a = mas[i][0] + 1;//For the second number of each array
+                mas[i][j] = num_a;
+            }
+            if (j >= 2)
+            {
+                num_a = mas[i][j - 1] + 1;//For the third+ number of each array
+                mas[i][j] = num_a;
+            }
+        }
+    }
+
+    cout << endl;
+
+    for (int i = 0; i < SIZE; i++)
+    {
+        for (int j = 0; j < SIZE; j++)
+        {
+            cout << mas[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
